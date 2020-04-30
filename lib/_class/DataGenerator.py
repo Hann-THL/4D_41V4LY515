02e8a597c65eefa5e0d5ed3d65c7b557418ff1ab
Hann-THL <hann_lim@hotmail.com>
2020-04-30 22:28:46 +0800

    update visualization script, bug fixing on target data type

--- lib/_class/DataGenerator.py
+++ lib/_class/DataGenerator.py
@@ -4,16 +4,16 @@ from tensorflow.keras.utils import Sequence
 
 # Reference: https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly
 class DataGenerator(Sequence):
-    def __init__(self, inputs, targets, batch_size):
-        self.inputs     = inputs
-        self.targets    = targets
+    def __init__(self, X, y, batch_size):
+        self.X = X
+        self.y = y
         self.batch_size = batch_size
         
     def __len__(self):
-        return int(np.ceil(len(self.inputs) / self.batch_size))
+        return int(np.ceil(len(self.X) / self.batch_size))
     
     def __getitem__(self, index):
-        batch_inputs  = self.inputs [index * self.batch_size : (index + 1) * self.batch_size]
-        batch_targets = self.targets[index * self.batch_size : (index + 1) * self.batch_size]
+        batch_X = self.X[index * self.batch_size : (index + 1) * self.batch_size]
+        batch_y = self.y[index * self.batch_size : (index + 1) * self.batch_size]
         
-        return batch_inputs, batch_targets
+        return batch_X, batch_y
